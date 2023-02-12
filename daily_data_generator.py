d8312cf3cde14da3d8abee737b4a1363214e8f10
Joycemaolol <91842873+Joycemaolol@users.noreply.github.com>
2023-02-12 12:18:37 -0500

    Cleaned the data a bit more, a prototype of MLP

--- daily_data_generator.py
+++ daily_data_generator.py
@@ -9,7 +9,13 @@ condition_location = "C:/Users/18jm6/OneDrive - Queen's University/2023 Winter/Q
 output_file_saving_location = "C:/Users/18jm6/OneDrive - Queen's University/2023 Winter/QMIND 2022-2023/"
 
 delta_t = 10 #this means corsaring the data by averaging the data over this time interval
-
+def zero_remover(daily_activity):
+    counts = daily_activity.value_counts()
+    werid = counts.max()
+    if werid > len(daily_activity)*0.8:
+        return False
+    else:
+        return True
 
 def daily_data_averager(daily_data, time_interval_in_minute):
     """
@@ -40,22 +46,28 @@ def daily_data_generator_condition(j, time_interval_in_minute):
     daily_motion_j[0] = daily_data_averager(extract_first_day, time_interval_in_minute)
     daily_motion_j.reset_index()
 
-    for i in range(2, len(day_splitter)-2):
+    for i in range(1, len(day_splitter)-1):
         start = day_splitter[i]
         end = day_splitter[i+1]
         extract_day_activity = test_file["activity"].loc[start:end-1]
-        #extract_day_time = temp[0]
-        daily_motion_j[i-1] = daily_data_averager(extract_day_activity, time_interval_in_minute)
+        if zero_remover(extract_day_activity):
+            #extract_day_time = temp[0]
+            try:
+                daily_motion_j[i-1] = daily_data_averager(extract_day_activity, time_interval_in_minute)
+            except ValueError:
+                pass
+        else:
+            pass
         
     daily_motion_j.reset_index()
     daily_motion_j = daily_motion_j.transpose()
-    daily_motion_j.columns = np.arange(0,  daily_motion_j.shape[1], 1)
+    daily_motion_j.columns = [str(i) for i in range(0, daily_motion_j.shape[1])]
 
     #add time 
-    temp = test_file["timestamp"].iloc[day_splitter]
-    daily_motion_j["Time"] = temp.dt.date.iloc[1:-2].to_list()
-    first_column = daily_motion_j.pop('Time')
-    daily_motion_j.insert(0, "Time", first_column)
+    #temp = test_file["timestamp"].iloc[day_splitter]
+    #daily_motion_j["Time"] = temp.dt.date.iloc[1:-1].to_list()
+    #first_column = daily_motion_j.pop('Time')
+    #daily_motion_j.insert(0, "Time", first_column)
 
     #add other attributes from scores.csv into the daily acitivity dataframe 
     temp_row_index = score_file.index[score_file["number"] == "condition_"+str(j)]
@@ -70,6 +82,7 @@ def daily_data_generator_condition(j, time_interval_in_minute):
     daily_motion_j["work"] = np.repeat(temp_row.iloc[0, 9], daily_motion_j.shape[0])
     daily_motion_j["madrs1"] = np.repeat(temp_row.iloc[0, 10], daily_motion_j.shape[0])
     daily_motion_j["madrs2"] = np.repeat(temp_row.iloc[0, 11], daily_motion_j.shape[0])
+    daily_motion_j["name"] = np.repeat("condition_"+str(j), daily_motion_j.shape[0])
 
     return daily_motion_j
 
@@ -94,22 +107,28 @@ def daily_data_generator_control(j, time_interval_in_minute):
     daily_motion_j[0] = daily_data_averager(extract_first_day, time_interval_in_minute)
     daily_motion_j.reset_index()
 
-    for i in range(2, len(day_splitter)-2):
+    for i in range(1, len(day_splitter)-1):
         start = day_splitter[i]
         end = day_splitter[i+1]
         extract_day_activity = test_file["activity"].loc[start:end-1]
-        #extract_day_time = temp[0]
-        daily_motion_j[i-1] = daily_data_averager(extract_day_activity, time_interval_in_minute)
+        if zero_remover(extract_day_activity):
+            #extract_day_time = temp[0]
+            try:
+                daily_motion_j[i-1] = daily_data_averager(extract_day_activity, time_interval_in_minute)
+            except ValueError:
+                pass
+        else:
+            pass
         
     daily_motion_j.reset_index()
     daily_motion_j = daily_motion_j.transpose()
-    daily_motion_j.columns = np.arange(0,  daily_motion_j.shape[1], 1)
+    daily_motion_j.columns = [str(i) for i in range(0, daily_motion_j.shape[1])]
 
     #add time 
-    temp = test_file["timestamp"].iloc[day_splitter]
-    daily_motion_j["Time"] = temp.dt.date.iloc[1:-2].to_list()
-    first_column = daily_motion_j.pop('Time')
-    daily_motion_j.insert(0, "Time", first_column)
+    #temp = test_file["timestamp"].iloc[day_splitter]
+    #daily_motion_j["Time"] = temp.dt.date.iloc[1:-1].to_list()
+    #first_column = daily_motion_j.pop('Time')
+    #daily_motion_j.insert(0, "Time", first_column)
 
     #add other attributes from scores.csv into the daily acitivity dataframe 
     temp_row_index = score_file.index[score_file["number"] == "control_"+str(j)]
@@ -124,7 +143,7 @@ def daily_data_generator_control(j, time_interval_in_minute):
     daily_motion_j["work"] = np.repeat(temp_row.iloc[0, 9], daily_motion_j.shape[0])
     daily_motion_j["madrs1"] = np.repeat(temp_row.iloc[0, 10], daily_motion_j.shape[0])
     daily_motion_j["madrs2"] = np.repeat(temp_row.iloc[0, 11], daily_motion_j.shape[0])
-
+    daily_motion_j["name"] = np.repeat("control_"+str(j), daily_motion_j.shape[0])
     return daily_motion_j
 
 
@@ -138,12 +157,36 @@ for k in condition_number:
     daily_motion = pd.concat([daily_motion, daily_data_generator_condition(k, delta_t)], axis = 0)
 
 for h in control_number:
-    daily_motion = pd.concat([daily_motion, daily_data_generator_control(k, delta_t)], axis = 0)
+    daily_motion = pd.concat([daily_motion, daily_data_generator_control(h, delta_t)], axis = 0)
+    
 
-
-#shuffle all the rows, reset index, and save
-daily_motion = daily_motion.sample(frac = 1)
+#reset index, and save
+#daily_motion = daily_motion.sample(frac = 1)
 daily_motion = daily_motion.set_index(np.arange(0, daily_motion.shape[0], 1))
-daily_motion.to_csv(output_file_saving_location+"daily_motion.csv") #the name of output file is daily_motion
+daily_motion.to_pickle(output_file_saving_location+"daily_motion.pickle") #the name of output file is daily_motion
 
+
+
+#create train and test dataset using different group of patients
+daily_motion_train = pd.DataFrame()
+daily_motion_test = pd.DataFrame()
+train_control_number = range(7, 32+1)
+test_control_number = range(1, 9)
+train_condition_number = range(7, 23+1)
+test_condition_number = range(1, 7)
+for k in train_condition_number:
+    daily_motion_train = pd.concat([daily_motion_train, daily_data_generator_condition(k, delta_t)], axis = 0)
+
+for h in test_condition_number:
+    daily_motion_test = pd.concat([daily_motion_test, daily_data_generator_condition(h, delta_t)], axis = 0)
+    
+for k in train_control_number:
+    daily_motion_train = pd.concat([daily_motion_train, daily_data_generator_control(k, delta_t)], axis = 0)
+
+for h in test_control_number:
+    daily_motion_test = pd.concat([daily_motion_test, daily_data_generator_control(h, delta_t)], axis = 0)
+    
+
+daily_motion_train.to_pickle(output_file_saving_location+"daily_motion_train.pickle") #the name of output file is daily_motion
+daily_motion_test.to_pickle(output_file_saving_location+"daily_motion_test.pickle") #the name of output file is daily_motion
 
