c90d7a1559d706b3305cc09db0455063a8b17946
Aria <102880878+AriaMaz@users.noreply.github.com>
2023-03-24 16:12:23 -0400

    Update JpcPlayground.py

--- JpcPlayground.py
+++ JpcPlayground.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt;
 Mydata = pd.read_csv('/Users/julienchanel/Documents/GitHub/data/scores.csv')
 Mydata
 
-## - Show dataset, create new columns with two existing variables
+## - Show dataset and create new columns with two existing variables
 
 Mydata.info()
 Mydata['Average madrs'] = ((Mydata.madrs2 + Mydata.madrs1)/2)
@@ -16,7 +16,7 @@ Mydata
 
 ## - Normalize the data [xnormalized = (x - xminimum) / range of x]
 
-## - plot the activity dataset (Q. How to import activity for all conditions - as their in seperate files. Does this mean we'll have to do our analysis from condition to condition)
+## - Plot the activity dataset (Q. How to import activity for all conditions - as their in seperate files. Does this mean we'll have to do our analysis from condition to condition)
 
 Activity1 = pd.read_csv('/Users/julienchanel/Documents/GitHub/data/condition/condition_1.csv')
 Activity1
@@ -29,11 +29,11 @@ plt.grid()
 plt.show()
 plt.rcParams['figure.figsize'] = Activity1_plot
 
-## - find the length of “0” motion(a range, maybe 0-5) for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
+## - Find the length of “0” motion(a range, maybe 0-5) for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
 
-## - find the “maximum” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
+## - Find the “maximum” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
 
-## - find the “Mean (Average)” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
+## - Find the “Mean (Average)” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
 
 Ac1Sum = sum(Activity1.activity)
 Activity1['AverageMotion'] = (Ac1Sum/Activity1.count())
@@ -42,12 +42,7 @@ Activity1
 AverageMotion_plot = plt.rcParams['figure.figsize']
 AverageMotion_plot(x='AverageMotion')
 
-## - find the “standard deviation” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
+## - Find the “standard deviation” motion for each patient each day, for unipolar, bipolar, and healthy controls. For each patient, plot a graph: x asis: day, y axis: amount of time with no movement
 
 
-## - learn how to(it’s ok if you don’t know how to code this at this point) encode data: change categorical data into numerical data
-
-
-
-
-
+## - Learn how to(it’s ok if you don’t know how to code this at this point) encode data: change categorical data into numerical data
